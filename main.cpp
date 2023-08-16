4be8cd84eb240494e410853f2815539b76a7c82a
cjohnson <collin.g.johnson@gmail.com>
2023-08-16 10:46:44 -0400

    Example from 'What is C++'?

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 
+int add(int a, int b) {
+    return a + b;
+}
+
+class Calculator {
+public:
+    int multiply(int a, int b) {
+        return a * b;
+    }
+};
+
 int main() {
-    int number;
-    std::cout << "Enter an integer: ";
-    std::cin >> number;
-    std::cout << std::endl << "You entered: " << number << std::endl;
+    int x = 5;
+    int y = 3;
+
+    int sum = add(x, y);
+    std::cout << "Sum: " << sum << std::endl;
+
+    Calculator calc;
+    int product = calc.multiply(x, y);
+    std::cout << "Product: " << product << std::endl;
+
     return 0;
 }

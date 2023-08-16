3e78c95eee0217f58fde9be9615ec6c78aa8b379
cjohnson <collin.g.johnson@gmail.com>
2023-08-16 10:11:05 -0400

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int number;
+    std::cout << "Enter an integer: ";
+    std::cin >> number;
+    std::cout << std::endl << "You entered: " << number << std::endl;
+    return 0;
+}
